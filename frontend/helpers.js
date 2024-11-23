c3e2f513ffd8fb68134f0c8c868a00fe5e0097f9
Vinny <vincentni2007@gmail.com>
2024-11-23 14:50:14 -0500

    remaking the flashcard system, pushing to gh so i can access on desktop

--- frontend/helpers.js
+++ frontend/helpers.js
@@ -1,15 +1,67 @@
 import {singles, multis} from "./data.js"
 
-function generate_single()
+class Single_Question {
+    constructor(type, question, answer)
+    {
+        this.type = type
+        this.question = question
+        this.answer = answer
+    }
+}
+
+class Multi_Question {
+    constructor(type, prompt, A, B, C)
+    {
+        this.type = type
+        this.prompt = prompt
+        this.A = A
+        this.B = B
+        this.C = C
+    }
+}
+
+function obj_to_question_class(question)
 {
-    let index = Math.floor(Math.random() * singles.length)
-    return singles[index]
+    if (question.hasOwnProperty("prompt"))
+    {
+        let A = new Single_Question("single", question.A.question, question.A.answer)
+        let B = new Single_Question("single", question.B.question, question.B.answer)
+        let C = new Single_Question("single", question.C.question, question.C.answer)
+        return new Multi_Question("multi", question.prompt, A, B, C)
+    }
+
+    return new Single_Question("single", question.question, question.answer)
+}
+
+let index = -1; // accounts for the fist generate questions call
+let all_questions;
+
+function generate_next_question()
+{
+    index += 1;
+    return all_questions[index];
+}
+
+function get_prev_question() {
+    if (index == 0)
+    {
+        alert("Can not go back any further.")
+        return singles[0]
+    }
+
+    index -= 1;
+    return all_questions[index];
 }
 
 function populate_text_area(input_text)
 {
     let text_area = document.getElementById("text-area")
     text_area.innerText = input_text
+}
+
+function populate_prompt_area(input_text)
+{
+    document.getElementById("prompt-area").innerText = input_text
 }
 
 function shuffle_array(array) {
@@ -20,13 +72,28 @@ function shuffle_array(array) {
 }
 
 function init() {
+    for (let i = 0; i < singles.length; i++)
+    {
+        singles[i] = obj_to_question_class(singles[i])
+    }
+
+    for (let i = 0; i < multis.length; i++)
+    {
+        multis[i] = obj_to_question_class(multis[i])
+    }
+
+    all_questions = singles.concat(multis)
+
     shuffle_array(singles)
     shuffle_array(multis)
+    shuffle_array(all_questions)
 }
 
 export {
-    generate_single,
+    generate_next_question,
     populate_text_area,
     shuffle_array,
+    get_prev_question,
+    populate_prompt_area,
     init
 }
--- frontend/main.js
+++ frontend/main.js
@@ -1,15 +1,54 @@
-import {generate_single, populate_text_area, shuffle_array, init} from "./helpers.js"
+import {generate_next_question, get_prev_question, populate_text_area, populate_prompt_area, init} from "./helpers.js"
 
 init()
 
 let current_question;
 let showing_answer = false;
+let multi_question_index = -1; // -1 = single question, 0 = A, 1 = B, 2 = C
 
-window.get_next_question = function() {
-    current_question = generate_single()
-    populate_text_area(current_question.question)
-    showing_answer = false;
+document.get_next_question() = function()
+{
+    if (multi_question_index == 2 || multi_question_index == -1)
+    {
+        current_question = generate_next_question()
+        if (current_question.hasOwnProperty("prompt"))
+        {
+            populate_prompt_area("Question prompt: " + current_question.prompt)
+            multi_question_index = 0
+        }
+    }
+
+    if (multi_question_index == -1)
+    {
+        populate_text_area(question.question)
+        showing_answer = false
+    }
 }
+
+// window.get_next_question = function() {
+//     current_question = generate_next_question()
+//     let prompt_area = document.getElementById("prompt-area")
+    
+//     if (current_question.hasOwnProperty("prompt"))
+//     {
+//         prompt_area.innerText = "Question Prompt: " + current_question.prompt
+//         showing_mutli_question = true;
+//     }
+//     else
+//     {
+//         prompt_area.innerText = ""
+//         showing_mutli_question = false;
+//     }
+    
+//     populate_text_area(current_question.question)
+//     showing_answer = false;
+// }
+
+// window.get_previous_question = function() {
+//     current_question = get_prev_question()
+//     populate_text_area(current_question.question)
+//     showing_answer = false
+// }
 
 window.flip_card = function() {
     if (showing_answer) {
